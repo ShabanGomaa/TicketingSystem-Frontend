a26e3da211862d32e5964b13e8e205224f31f0e3
Shaban Gomaa <shaban_gomaa@hotmail.com>
2024-10-11 04:14:09 +0300

    Fix tooltip and canvas

---
+++ README.md
@@ -0,0 +1,3 @@
+ng build --output-path=dist/browser --base-href "https://ShabanGomaa.github.io/TicketingSystem-Frontend/"
+
+npx angular-cli-ghpages --dir=dist/browser
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
 if (environment.production) {
     enableProdMode();
 }
@@ -12,3 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     platformBrowserDynamic().bootstrapModule(AppModule)
         .catch(err => console.error(err));
 });
+
+bootstrapApplication(AppComponent, {
+    providers: [provideCharts(withDefaultRegisterables())],
+}).catch((err) => console.error(err));
